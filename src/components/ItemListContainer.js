a4d72e036320ce5754e9b6e38a0e5e8d1427022e
verteramo.federico <verteramo.federico@gmail.com>
2021-08-26 16:24:03 -0300

    se agrega fetch()

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,14 +2,21 @@ import ItemCount from './ItemCount';
 import ItemList from './ItemList'; 
 import {useState, useEffect} from "react"
 
+const URL_API_PRODUCTS = "https://mocki.io/v1/85f13fc4-5212-4796-a403-994e8ef8280e"
+
+
+// manera manual antes de usar mocki.io
 const singleItem = () =>{
     return new Promise((resolve, reject) => {
     setTimeout(
         () => 
         resolve([
+            {id: 1,title: 'casco',destacado: false, description: 'casco amarillo',price: 1100,pictureUrl: 'uploads/products/casco.jpg'},
             {id: 2,title: 'pantalon',destacado: true, description: 'pantalon largo',price: 1900,pictureUrl: 'uploads/products/pantalon.jpg'},
-            {id: 3,title: 'camisa',destacado: false, description: 'camisa mangas',price: 1600,pictureUrl: 'uploads/products/camisa.jpg'},
-            {id: 4,title: 'casco',destacado: true, description: 'casco amarillo',price: 1100,pictureUrl: 'uploads/products/casco.jpg'}         
+            {id: 3,title: 'camisa',destacado: true, description: 'camisa mangas',price: 1600,pictureUrl: 'uploads/products/camisa.jpg'},
+            {id: 4,title: 'Botin',destacado: false, description: 'botin de trabajo',price: 1100,pictureUrl: 'uploads/products/botin.jpg'}, 
+            {id: 5,title: 'Guante',destacado: true, description: 'guante tejido moteado',price: 1400,pictureUrl: 'uploads/products/guante.jpg'}, 
+            {id: 6,title: 'Zapato',destacado: false, description: 'zapato de seguridad',price: 1600,pictureUrl: 'uploads/products/zapato.jpg'}      
             ]),
             2000);
             })}
@@ -20,7 +27,10 @@ const ItemListContainer = (props) => {
     let [ ItemsArray, cambioState ] = useState([]) 
 
     useEffect(() => {
-        singleItem().then((data) => {
+        //singleItem()
+        fetch(URL_API_PRODUCTS)
+        .then(response => response.json())
+        .then((data) => {
             let aux = data.filter((producto) => producto.destacado);
             cambioState(aux);
             console.log(ItemsArray)
